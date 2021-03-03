9f2284997656ecd40cedd4efc33409839829bbfd
Taylor Dickinson <cisco.cat@hotmail.com>
2021-03-03 00:35:56 -0500

    Added new and wake endpoints

--- app/server.ts
+++ app/server.ts
@@ -1,8 +1,39 @@
 const http = require('http');
+import * as express from "express"
+const app = express();
 
-let app = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World!\n');
+import * as wol from "wake_on_lan";
+
+
+// let app = http.createServer((req, res) => {
+//     res.writeHead(200, {'Content-Type': 'text/plain'});
+//     res.end('Hello World!\n');
+// });
+
+app.get('/', (req, res) => {
+    res.send("Hello World");
 });
-app.listen(1800);
-console.log('Node server running on port 1800');
+app.get('/new', (req, res) => {
+    res.send("Wow");
+});
+app.post('/wake', (req, res) => {
+    try {
+        wol.wake("00-24-8C-5A-70-74", { address: "192.168.1.6", port: 7 }, (err) => {
+            if (err) {
+                console.log(err);
+                return res.status(500).end();
+            } else {
+                console.log("Success");
+                return res.status(200).end();
+            }
+
+        })
+    } catch (err) {
+        return res.status(500).end();
+    }
+});
+
+
+app.listen(1800, () => {
+    console.log('Node server running on port 1800');
+});
