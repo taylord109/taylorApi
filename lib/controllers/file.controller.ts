be11a71da02d7c60ebd02a7226d8471314711be0
Taylor Dickinson <cisco.cat@hotmail.com>
2021-03-20 05:00:47 -0400

    Updated file error message

--- lib/controllers/file.controller.ts
+++ lib/controllers/file.controller.ts
@@ -38,9 +38,9 @@ export class FileController {
 
     public getDownloadFile(req: Request, res: Response) {
         if (!req?.params?.fileName) return res.status(400).send("No file found in db").end();
-        File.findOne({ name: "Ashen Inquisitor A - Supported" }, async (err, files) => {
-            if (err) return res.send(err).end();
-            if (!files || !files["_doc"] || !files["_doc"]["path"]) return res.send("No file found in db").end();
+        File.findOne({ name: "Ashen Inquisitor A - Supported" }, async (err: mongoose.CallbackError, files) => {
+            if (err) return res.status(400).send(err.message).end();
+            if (!files || !files["_doc"] || !files["_doc"]["path"]) return res.status(400).send("No file found in db").end();
             let file = files._doc;
             const path: string = "/var/external" + file.path;
             var hasFile = await new Promise<boolean>((resolve, reject) => {
